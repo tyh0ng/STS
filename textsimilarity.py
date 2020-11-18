8542fd2551c2cd9358701d84c1a3b32fa80c2e4e
Tyler Hong <tylerw.hong@gmail.com>
2020-11-18 05:14:43 +0000

    done with sts scores

--- textsimilarity.py
+++ textsimilarity.py
@@ -11,14 +11,14 @@ if torch.cuda.is_available():
 # model = SentenceTransformer("xlm-r-bert-base-nli-stsb-mean-tokens")
 # model = SentenceTransformer("distilbert-multilingual-nli-stsb-quora-ranking")
 # model = SentenceTransformer("LaBSE")
-tsv_file = open('eval.tsv')
-read_tsv = csv.reader(tsv_file, delimiter="\t")
+tsv_file = open('train-hotels-es.csv')
+read_tsv = csv.reader(tsv_file, delimiter=",")
 src_sentences = []
 trg_sentences = []
 
 for row in read_tsv:
-  src_sentences.append(row[0])
-  trg_sentences.append(row[1])
+  src_sentences.append(row[1])
+  trg_sentences.append(row[2])
 batch_size = 500
 embeddings1 = model.encode(src_sentences, batch_size=batch_size, show_progress_bar=True,
                            convert_to_numpy=True)
@@ -28,7 +28,7 @@ cosine_scores = 1 - (paired_cosine_distances(embeddings1, embeddings2))
 print('cosine_scores', cosine_scores)
 
 # name of csv file  
-filename = "final_spanish_results1.csv"
+filename = "train-hotels-es-sts-scores.csv"
     
 # writing to csv file  
 with open(filename, 'w') as csvfile:  
